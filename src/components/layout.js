60da08005a2386e6e1d0e2dd59f3328809bf6c74
vj-abishek <abigosearch@gmail.com>
2020-12-03 15:13:17 +0530

    UI changes

--- src/components/layout.js
+++ src/components/layout.js
@@ -28,7 +28,7 @@ const Layout = ({ children }) => {
       <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
       <div className="w-full min-h-screen max-h-full max-w-full overflow-hidden bg-gray-100 dark:bg-gray-900 font-sans text-base flex flex-col md:justify-center items-center">
         <main>{children}</main>
-        <footer className="w-full bg-gray-700 dark:bg-black text-center p-2 text-white">
+        <footer className="w-full bg-gray-700 dark:bg-black text-center fixed bottom-0 py-2 text-white">
           Built by <a href="https://twitter.com/abishek_py">@abishek_py</a>
         </footer>
       </div>
